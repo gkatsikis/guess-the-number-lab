bdefd22412a465b0be44d39cf0b069c92b8f7dc6
gkatsikis <geo.bjj.kat@gmail.com>
2022-01-28 18:46:41 -0500

    basic instructions and setup

--- app.js
+++ app.js
@@ -11,4 +11,26 @@ const game = {
   }
 }
 
+let prevGuesses = [] //to compile past guesses to display
+
+
+let getGuess = prompt(`Guess a number between ${[smallestNum]} and ${[biggestNum]}`)
+
+//while loop to ensure 1.number not a string and 2.is between smallestNum and biggestNum
+//parseInt returns NaN if string cannot become a number
+
+//try console.log play method to see what happens
+
+//use play method to invoke getGuess from inside a loop and add new guess to prevGuesses array
+
+//create render method to game that play will call after a guess has been made
+//will alert if guess is correct
+//OR if guess is too high or low and previous guesses
+
+//'return' only when player has guessed secretNum
+
+window.alert("Sorry! That number wasn't quite right") //for incorrect guesses -- add too high or too low depending
+
+
+
 
