46958ff8494d578149799ff194ebb58b7db21c0d
gkatsikis <geo.bjj.kat@gmail.com>
2022-01-29 16:25:41 -0500

    render function active

--- app.js
+++ app.js
@@ -6,21 +6,23 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
-  displayGuesses: [],
 
   play: function () {
+    // this.biggestNum = prompt('Choose the maximum number')
+    // this.smallestNum = prompt('Choose the lowest number')
+    //^^^ this gets to getGuess() but doesn't return console.log or array
     this.secretNum = Math.floor(Math.random() *
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     do {
       this.getGuess()
-      // for(let i = 0; i < this.prevGuesses.length; i++){
-      //   if(this.prevGuesses[i] !== NaN){
-      //     this.displayGuesses.push(this.prevGuesses[i])
-      //   }
-      // }
+      if (guess > this.secretNum){
+        console.log('Your guess was too high')
+      } else if(guess < this.secretNum){
+        console.log('Your guess was too low')
+      }
       console.log(this.prevGuesses)
     } while (guess !== this.secretNum)
-    console.log(`You win! The number was ${this.secretNum}!`)
+    this.render()
    },
 
   getGuess: function () {
@@ -35,41 +37,21 @@ const game = {
     while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) {
       console.log("Nice try! But that either wasn't within the numerical range or not a number")
       this.getGuess()
-      this.prevGuesses.push(guess)
     }
+    this.prevGuesses.push(guess)
   },
 
     render: function(){
-      if(guess === this.secretNum){
         console.log(`Congratulations! You guessed it! The number was ${this.secretNum}`)
+        numGuesses = this.prevGuesses.length
+        console.log(`It took you ${numGuesses} guesses`)
       } 
-    }
   }
 
+  // game.play()
 
-game.play()
-
-
-
-
-
-
-//while loop to ensure 1.number not a string and 2.is between smallestNum and biggestNum
-//parseInt returns NaN if string cannot become a number
-
-//try console.log play method to see what happens
-
-//use play method to invoke getGuess from inside a loop and add new guess to prevGuesses array
-
-//create render method to game that play will call after a guess has been made
-//will alert if guess is correct
-//OR if guess is too high or low and previous guesses
-
-//'return' only when player has guessed secretNum
-
-// window.alert("Sorry! That number wasn't quite right") ***
-//for incorrect guesses -- add too high or too low depending
-
-
+//Task1: Array of PrevGuesses >>> .join() previous guesses
+//Why is it ending then resetting?
+//variable not creating with 'let' and 'const', only alone
 
 
